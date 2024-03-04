c4eb3ae842c1af3fe85aa804e2af3b9c50aa1ba2
Mohamed.Hisham <mohamedhisham714@gmail.com>
2024-03-04 14:32:09 +0200

    Adding

--- app/src/main/java/mo/zain/biometric/MainActivity.kt
+++ app/src/main/java/mo/zain/biometric/MainActivity.kt
@@ -65,3 +65,63 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
+/*
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import java.io.File
+import java.io.IOException
+
+fun updateUserInfo(token: String, files: List<Pair<String, String>>) {
+    val url = "http://127.0.0.1:8000/api/update_user_infoo/"
+
+    val client = OkHttpClient()
+
+    val requestBodyBuilder = MultipartBody.Builder().setType(MultipartBody.FORM)
+    requestBodyBuilder.addFormDataPart("email", "omn@example.com")
+    requestBodyBuilder.addFormDataPart("phone", "01002284852")
+    requestBodyBuilder.addFormDataPart("fullname", "zzzxx")
+
+    files.forEach { (name, filePath) ->
+        val file = File(filePath)
+        if (file.exists()) {
+            requestBodyBuilder.addFormDataPart(
+                "attachments", // Ensure the parameter name is correct
+                file.name,
+                RequestBody.create("image/png".toMediaTypeOrNull(), file)
+            )
+        } else {
+            println("File not found: $filePath")
+        }
+    }
+
+    val requestBody = requestBodyBuilder.build()
+
+    val request = Request.Builder()
+        .url(url)
+        .header("Authorization", "Token $token")
+        .put(requestBody)
+        .build()
+
+    client.newCall(request).enqueue(object : Callback {
+        override fun onFailure(call: Call, e: IOException) {
+            e.printStackTrace()
+            println("An error occurred: ${e.message}")
+        }
+
+        override fun onResponse(call: Call, response: Response) {
+            val responseBody = response.body?.string()
+            println(responseBody)
+        }
+    })
+}
+
+fun main() {
+    val token = "28a9a6b87c1d39913d528a70635256ddd27be5dc"
+    val files = listOf(
+        Pair("photo", "D:\\testyy\\img.PNG"),
+        Pair("passport", "D:\\testyy\\img2.PNG"),
+        // Add more files as needed
+    )
+    updateUserInfo(token, files)
+}
+ */
